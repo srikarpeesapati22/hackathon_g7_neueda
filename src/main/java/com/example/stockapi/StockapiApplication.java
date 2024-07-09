5a75af644335674adbc3602fbc3bea05c528ba00
srikarpeesapati22 <112534138+srikarpeesapati22@users.noreply.github.com>
2024-07-09 20:07:01 +0100

    Main app

--- src/main/java/com/example/stockapi/StockapiApplication.java
+++ src/main/java/com/example/stockapi/StockapiApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StockapiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(StockapiApplication.class, args);
 	}
-
 }
